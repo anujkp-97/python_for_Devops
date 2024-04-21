8a71bc62007cf94605336315f6adadbc08997969
ANUJ KUMAR <122070756+anujkp-97@users.noreply.github.com>
2024-04-21 10:31:34 +0000

    practice

---
+++ practice_mis/function.py
@@ -0,0 +1,7 @@
+def address(city, state, country):
+    # city = input("Enter your city: ")
+    # state = input("Enter your state name: ")
+    # country = input("Country name:")
+    print("Your are from", city,state,country)
+
+address("MBD", "UP","Bharat")
---
+++ practice_mis/loop_while.py
@@ -0,0 +1,10 @@
+count=0
+while count<5:
+    count+=1
+    print("Iteration no. {}".format(count))
+    if(count ==1):
+       continue
+else:
+    print("Loop is over")
+
+print("End of while loop")
